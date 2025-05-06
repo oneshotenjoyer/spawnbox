62b1baba0de52560326293d3c4e7b183d2f40ff7
modguy07 <206636428+oneshotenjoyer@users.noreply.github.com>
2025-05-05 20:24:48 -0400

    Tweak something + TOML

--- src/spawnbox/entity.py
+++ src/spawnbox/entity.py
@@ -1,5 +1,7 @@
 import sdl2, math
 
+from .main import SpawnBox
+
 '''
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -7,7 +9,7 @@ import sdl2, math
 '''
 
 class Entity(object):
-    def __init__(self, window, width = 20, height = 20, x = 0, y = 0, color = (255, 255, 255, 255)):
+    def __init__(self, window: SpawnBox, width = 20, height = 20, x = 0, y = 0, color = (255, 255, 255, 255)):
         self.window = window
         self.width = width
         self.height = height
@@ -21,15 +23,15 @@ class Entity(object):
         sdl2.SDL_RenderDrawRectF(self.window.renderer, self.rect)
         sdl2.SDL_RenderFillRectF(self.window.renderer, self.rect)
         self.rect = sdl2.SDL_FRect(self.x, self.y, self.width, self.height)
-    def collide(self, rect):
+    def collide(self, ent):
         leftA = self.rect.x
         rightA = self.rect.x + self.rect.w
         topA = self.rect.y
         bottomA = self.rect.y + self.rect.h
-        leftB = rect.rect.x
-        rightB = rect.rect.x + rect.rect.w
-        topB = rect.rect.y
-        bottomB = rect.rect.y + rect.rect.h
+        leftB = ent.rect.x
+        rightB = ent.rect.x + ent.rect.w
+        topB = ent.rect.y
+        bottomB = ent.rect.y + ent.rect.h
         if bottomA <= topB:
             return False
         if topA >= bottomB:
@@ -46,5 +48,5 @@ class Entity(object):
         self.x = self.window.width / 2 - self.width / 2
     def center_y(self):
         self.y = self.window.height / 2 - self.height / 2
-    def distance(rect1, rect2):
-        return (round(math.sqrt((rect2.x - rect1.x) ** 2 + (rect2.y - rect1.y) ** 2), 2)) / 10
+    def distance(ent1, ent2):
+        return (round(math.sqrt((ent2.x - ent1.x) ** 2 + (ent2.y - ent1.y) ** 2), 2)) / 10
